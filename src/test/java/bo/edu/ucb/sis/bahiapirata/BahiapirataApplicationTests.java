bb2ee6d25d9ac3b4e042e28be20ca524837c2b6e
zphoenixz <ing.ramiro.valdez.a@gmail.com>
2020-03-22 12:19:54 -0400

    project setup

---
+++ src/test/java/bo/edu/ucb/sis/bahiapirata/BahiapirataApplicationTests.java
@@ -0,0 +1,13 @@
+package bo.edu.ucb.sis.bahiapirata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BahiapirataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
